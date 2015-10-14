3f8a9801d3257b065bf39e912461640ace89577f
xzhofan <fan.zhong@tieto.com>
2015-10-14 17:36:43 +0800

    change layout

--- handwriteppt/src/com/github/handwriteppt/DrawPad.java
+++ handwriteppt/src/com/github/handwriteppt/DrawPad.java
@@ -1,12 +1,17 @@
 package com.github.handwriteppt;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.HeadlessException;
 import java.io.File;
 import java.util.Properties;
 
 import javax.swing.DefaultListModel;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
@@ -20,9 +25,20 @@ public class DrawPad extends JFrame
   private int                  currentPageIndex = 0;
   private PagesList            pagesList;
   private LayerList            layersList;
+  private JLabel               addNewPageLabel;
+  private JLabel               addNewLayerLabel;
+  private JLabel               showLabel;
+  private JLabel               saveLabel;
+  private JLabel               strokeLabel;
+  private JLabel               blankLabel;
+  private StrokeSpinner        stroke;
+  private JLabel               rubberLabel;
+  private JLabel               colorSelectorLabel;
   private String               saveFolder;
   private String               fileNameFormat   = "%s\\%d-%d.png";
   private static final DrawPad instance         = new DrawPad("Hand Write Board");
+  private GridBagLayout        layout;
+  private GridBagConstraints   cons;
 
   {
     try
@@ -64,7 +80,20 @@ public class DrawPad extends JFrame
 
   private void initialUiComps()
   {
-    drawToolbar = new DrawPadToolBar(this);
+    layout = new GridBagLayout();
+    setLayout(layout);
+    cons = new GridBagConstraints();
+    cons.fill = GridBagConstraints.NONE;
+    //drawToolbar = new DrawPadToolBar(this);
+    addNewPageLabel = new NewPageLabel(new ImageIcon("res/AddPage.png"), this);
+    addNewLayerLabel = new NewLayerLabel(new ImageIcon("res/AddLayer.png"), this);
+    rubberLabel = new RubberLabel(new ImageIcon("res/Eraser.png"), this);
+    showLabel = new ShowLabel(new ImageIcon("res/show.png"), this);
+    saveLabel = new SaveLabel(new ImageIcon("res/save.png"), this);
+    colorSelectorLabel = new JLabel(new ImageIcon("res/Paint.png"));
+    strokeLabel = new JLabel(new ImageIcon("res/Pen.png"));
+    stroke = new StrokeSpinner(1.0, 1.0, 20.0, 0.1);
+    blankLabel = new JLabel();
     pagesList = new PagesList(this);
     layersList = new LayerList(this);
     loadPagesFromFile("");
@@ -76,13 +105,49 @@ public class DrawPad extends JFrame
     {
       addNewPage();
     }
-    add(drawToolbar, BorderLayout.NORTH);
-    add(pagesList, BorderLayout.WEST);
-    add(layersList, BorderLayout.EAST);
+    add(saveLabel);
+    setLayoutCons(saveLabel, 0, 0, 1, 1, 1, 1);
+    add(showLabel);
+    setLayoutCons(showLabel, 1, 0, 1, 1, 1, 1);
+    add(rubberLabel);
+    setLayoutCons(rubberLabel, 2, 0, 1, 1, 1, 1);
+    add(colorSelectorLabel);
+    setLayoutCons(colorSelectorLabel, 3, 0, 1, 1, 1, 1);
+    add(strokeLabel);
+    setLayoutCons(strokeLabel, 4, 0, 1, 1, 1, 1);
+    add(stroke);
+    setLayoutCons(stroke, 5, 0, 1, 1, 1, 1);
+    add(addNewPageLabel);
+    setLayoutCons(addNewPageLabel, 0, 1, 1, 1, 1, 1);
+    add(blankLabel);
+    setLayoutCons(blankLabel, 1, 1, 4, 1, 1, 1);
+    add(addNewLayerLabel);
+    setLayoutCons(addNewLayerLabel, 5, 1, 1, 1, 1, 1);
+    add(pagesList);
+    setLayoutCons(pagesList, 0, 2, 1, 4, 1, 1);
+    add(pagesList.getModel().get(0));
+    setLayoutCons(pagesList.getModel().get(0), 1, 2, 4, 4, 100, 100);
+    add(layersList);
+    setLayoutCons(layersList, 5, 2, 1, 4, 1, 1);
+
+    //    add(drawToolbar, BorderLayout.NORTH);
+    //    add(pagesList, BorderLayout.WEST);
+    //    add(layersList, BorderLayout.EAST);
     setExtendedState(JFrame.MAXIMIZED_BOTH);
     setVisible(true);
     validate();
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  }
+
+  private void setLayoutCons(Component comp, int x, int y, int width, int height, int weightx, int weighty)
+  {
+    cons.gridx = x;
+    cons.gridy = y;
+    cons.gridwidth = width;
+    cons.gridheight = height;
+    cons.weightx = weightx;
+    cons.weighty = weighty;
+    layout.setConstraints(comp, cons);
   }
 
   private void loadPagesFromFile(String filePath)
@@ -103,7 +168,7 @@ public class DrawPad extends JFrame
     pagesList.setSelectedIndex(pagesList.getModel().size() - 1);
     layersList.setModel(pagesList.getSelectedValue().getLayers());
     layersList.setSelectedIndex(0);
-    add(addedPage, BorderLayout.CENTER);
+    //    add(addedPage, BorderLayout.CENTER);
   }
 
   public void switchPage(int index)
